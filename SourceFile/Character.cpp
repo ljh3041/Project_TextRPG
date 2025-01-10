bfda23a6ade96cf1afd26d2db7ea0533d2812dd6
ljh3041 <ljh3041@hanmail.net>
2025-01-10 17:43:42 +0900

    생성자 초기화 부분 수정

--- SourceFile/Character.cpp
+++ SourceFile/Character.cpp
@@ -1,7 +1,13 @@
 #include "../headerFile/Character.h"
 Character::Character()
 {
-	
+    name = "";
+    level = 1;
+    health = 100;
+    maxHealth = 100;
+    attack = 10;
+    experience = 0;
+    gold = 0;
 }
 
 Character::~Character()
