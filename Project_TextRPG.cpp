9efd38486177dd02623ed8a2517ca3af344cf409
ljh3041 <ljh3041@hanmail.net>
2025-01-10 15:38:15 +0900

    메인 함수 업데이트
    25.01.10 15:38

--- Project_TextRPG.cpp
+++ Project_TextRPG.cpp
@@ -30,14 +30,20 @@ GameManager* GameManager::instance = nullptr;
 
 int main()
 {
-    cout << "Main Loop" << endl;
 
     // 게임 매니저가 처리하는 부분.
     // 메인 루프 속에서 게임의 진행이 처리
+    int time = 0;
     while (1)
     {
+        cout << "================================" << endl;
         cout << "inGame" << endl;
+        cout << "진행 시간 : " << time << endl;
+        cout << "================================" << endl;
         Sleep(1000);
+
+        system("cls");
+        time++;
     }
 
 
