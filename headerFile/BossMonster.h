9833eab7c6c906f09f5d1a424ee322b6675f6697
이광섭 <rhkdtjqlove@naver.com>
2025-01-13 17:17:36 +0900

    Update : Monster constructor & New : decoword
    +Update : Monster constructor
    +New : decoword

--- headerFile/BossMonster.h
+++ headerFile/BossMonster.h
@@ -6,7 +6,6 @@ using namespace std;
 class BossMonster : public Monster
 {
 public:
-	BossMonster() : Monster("BossMonster") {}
+	BossMonster() : Monster("BossMonster") { name += " 드래곤"; }
 	BossMonster(const std::string& _name) : Monster(_name) {}
-	
 };
---
+++ headerFile/DecoWord.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "MyMath.h"
+using namespace std;
+
+class DecoWord
+{
+public:
+	vector<string> DecoWordList1;
+	vector<string> DecoWordList2;
+
+	string nickname(string _name);
+
+	static DecoWord& getInstance() {
+		static DecoWord instance; // 유일한 객체 생성
+		return instance;
+	}
+
+private:
+	DecoWord();
+	DecoWord(const DecoWord&) = delete;
+	DecoWord& operator=(const DecoWord&) = delete;
+};
