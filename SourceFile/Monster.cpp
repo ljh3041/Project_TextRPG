fc41a4de4748fb7770ae505767882341f786b48b
이광섭 <rhkdtjqlove@naver.com>
2025-01-15 10:27:36 +0900

    fix
    fix

--- SourceFile/Monster.cpp
+++ SourceFile/Monster.cpp
@@ -7,7 +7,7 @@ Monster::Monster(string _name)
 	if (it != monsterDatas.end())
 	{
 		DecoWord::getInstance().nickname(this->name);							//몬스터 데이터에 있으면 별명 먼저 넣어주기
-		
+
 		this->health =  GetRandomHealthStat(_name);	//그 몬스터의 체력계수와 레벨, 랜덤값을 계산한 최종 체력
 		this->attack =  GetRandomAttackStat(_name);;	//그 몬스터의 공격계수와 레벨, 랜덤값을 계산한 최종 공격력
 		health;
